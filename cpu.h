d6666465cce192f198a0f59bfa8ee35a0923bdb4
cr0sd <jumpingkanga@outlook.com>
2020-01-25 09:43:20 -0600

    cpu.h: fixed SR union with struct of bitfields

--- cpu.h
+++ cpu.h
@@ -12,24 +12,28 @@ typedef struct ram_t ram_t;
 
 typedef struct cpu_t
 {
-	int16_t pc;		// Program counter
-	int8_t ac;		// Accumulator
-	int8_t x;
-	int8_t y;
+	uint16_t pc;		// Program counter
+	uint8_t ac;		// Accumulator
+	uint8_t x;
+	uint8_t y;
 
 	union			// Status register
 	{
-		int n:1;	// Negative
-		int v:1;	// Overflow
-		int _:1;	// Ignored
-		int b:1;	// Break
-		int d:1;	// Decimal
-		int i:1;	// Interrupt (enable/disable)
-		int z:1;	// Zero
-		int c:1;	// Carry
+		uint8_t reg;	// Entire 8-bit register
+		struct
+		{
+			int n:1;	// Negative
+			int v:1;	// Overflow
+			int _:1;	// Ignored
+			int b:1;	// Break
+			int d:1;	// Decimal (BCD)
+			int i:1;	// Interrupt (enable/disable)
+			int z:1;	// Zero
+			int c:1;	// Carry
+		}bits;			// Access individual bit flags
 	}sr;
 
-	int8_t sp;		// Stack pointer
+	uint8_t sp;		// Stack pointer
 }cpu_t;
 
 cpu_t*cpu_init(void);
