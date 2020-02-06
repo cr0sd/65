81ae01579cb838e33e122af9730cd676cab88930
cr0sd <jumpingkanga@outlook.com>
2020-02-06 16:14:55 -0600

    disasm: multiple lines

--- disasm.c
+++ disasm.c
@@ -4,27 +4,37 @@ void da_print_disassembly(cpu_t*cpu,ram_t*ram)
 {
 	int y=8;
 
-	// Print offset
-	attron(COLOR_PAIR(3));
-	mvprintw(y,0,"%04x",cpu->pc);
-	attroff(COLOR_PAIR(3));
-
-	// Opcodes
-	switch(ram->ram[cpu->pc])
+	for(int i=0;i<4;++i)
 	{
 
-		case 0xa0: // ldy imm
-			printw( "\t%02x %02x", ram->ram[cpu->pc], ram->ram[cpu->pc+1] );
-			printw( "\tldy $%02x", ram->ram[cpu->pc+1] );
-			break;
+		// Print offset
+		attron(COLOR_PAIR(3));
+		mvprintw(y,0,"%04x",cpu->pc+i);
+		attroff(COLOR_PAIR(3));
 
-		case 0xea: // nop
-			printw( "\t%02x", ram->ram[cpu->pc], ram->ram[cpu->pc+1] );
-			printw( "\tnop" );
-			break;
+		// Opcodes
+		switch(ram->ram[cpu->pc+i])
+		{
 
-		default:
-			mvprintw(y,0,"-                               ");
+			case 0xa0: // ldy imm
+				printw( "\t%02x %02x", ram->ram[cpu->pc+i], ram->ram[cpu->pc+i+1] );
+				printw( "\tldy $%02x", ram->ram[cpu->pc+i+1] );
+				break;
 
+			case 0xa2: // ldx imm
+				printw( "\t%02x %02x", ram->ram[cpu->pc+i], ram->ram[cpu->pc+i+1] );
+				printw( "\tldx $%02x", ram->ram[cpu->pc+i+1] );
+				break;
+
+			case 0xea: // nop
+				printw( "\t%02x", ram->ram[cpu->pc+i], ram->ram[cpu->pc+i+1] );
+				printw( "\tnop" );
+				break;
+
+			default:
+				mvprintw(y,0,"-                               ");
+
+		}
+		++y;
 	}
 }
