df5f5e2a2e9e48650797332b0785f93e3a040a6e
cr0sd <jumpingkanga@outlook.com>
2020-02-07 07:24:52 -0600

    disasm: pre-calc imm values

--- disasm.c
+++ disasm.c
@@ -1,5 +1,6 @@
 #include"disasm.h"
 #include"stdlib.h"
+#include"stdarg.h"
 
 static int oper[8];
 
@@ -10,21 +11,43 @@ static void pi(cpu_t*cpu,ram_t*ram,int n,const char*fmt,...)
 	#undef __cpu
 	#define __cpu cpu
 
+	//printw("\t(pc:$%04x)\t",cpu->pc);
+
+	// pre calculate operands (because ABI is
+	// reversing the evaluation of stack-sensitive
+	// values:
+	int oper[3];
+	oper[0] = ram->ram[ cpu->pc ];
+
 	// Print binary representation
 	if(n==1)
-		printw( "\t%02x\t\t", imm() );
+	{
+		printw( "\t%02x\t\t", oper[0] );
+	}
 	else if(n==2)
-		printw( "\t%02x %02x\t\t", imm(), imm() );
+	{
+		oper[1]=imm();
+		printw( "\t%02x %02x\t\t", oper[0], oper[1] );
+	}
 	else if(n==3)
-		printw( "\t%02x %02x %02x\t", imm(), imm(), imm() );
+	{
+		oper[1]=imm();
+		oper[2]=imm();
+		printw( "\t%02x %02x %02x\t", oper[0], oper[1], oper[2] );
+	}
+	else if(n==0)
+		;
 
 	// Print rest of vararg stuff (insn name, operands)
 	va_list list;
 	va_start(list,fmt);
 	vwprintw(win,fmt,list);
 	va_end(list);
+
+	//printw(" (pc:$%04x)\t",cpu->pc);
 }
 
+#define p0(...) pi(cpuc,ram,0,__VA_ARGS__)
 #define p1(...) pi(cpuc,ram,1,__VA_ARGS__)
 #define p2(...) pi(cpuc,ram,2,__VA_ARGS__)
 #define p3(...) pi(cpuc,ram,3,__VA_ARGS__)
@@ -37,7 +60,7 @@ void da_print_disassembly(cpu_t*cpu,ram_t*ram)
 	#undef __cpu
 	#define __cpu cpuc
 
-	int y=8;
+	int y=8;	// Which line to draw on
 	cpu_t*cpuc=alloca(sizeof(cpu_t));
 
 	// Create copy of cpu
@@ -88,10 +111,14 @@ void da_print_disassembly(cpu_t*cpu,ram_t*ram)
 
 		default:
 			mvprintw(y,0,"-                                             ");
+			//p0("incpc(1)");
+			incpc(1);
+			//printw("\t(pc:$%04x)\t",cpuc->pc);
 		}
 
 		// Next line of disassembly
-		cpuc->pc+=1;
+		//cpuc->pc+=1;
+		incpc(1);
 		++y;
 	}
 }
