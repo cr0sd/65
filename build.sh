5da948496ad719181303244e53428bb656e4ca8c
cr0sd <jumpingkanga@outlook.com>
2020-05-05 10:38:30 -0500

    build.sh: added rake support

--- build.sh
+++ build.sh
@@ -14,34 +14,36 @@ if [ $(which dialog > /dev/null) -ne 0 ]; then
 fi
 
 # Choose make vs. rake
-dialog --title "$0" --radiolist "Choose make utility" $NV $NH 10 \
+dialog --title "Make utility" --radiolist "Choose make utility" $NV $NH 10 \
 	make "" on \
 	rake "" off \
 	2>$TMPFILE
-MAKECMD=$(cat .dialogtmpfile)
+MAKECMD=$(cat $TMPFILE)
 
 # Choose TARGET
-dialog --title "$0" --radiolist "Choose target" $NV $NH 10 \
+dialog --title "Target" --radiolist "Choose target" $NV $NH 10 \
 	all "all" on \
 	clean "clean" off \
 	2>$TMPFILE
-TARGET=$(cat .dialogtmpfile)
+TARGET=$(cat $TMPFILE)
 
 # Choose multi-threading/jobs options
-dialog --title "$0" --radiolist "Choose number of jobs/threads" $NV $NH 10 \
-	1 "1 thread (one job at a time)" off \
-	2 "2 threads" off \
-	4 "4 threads" on \
-	8 "8 threads" off \
-	2>$TMPFILE
-NJOBS=$(cat .dialogtmpfile)
+if [ $MAKECMD = "make" ]; then
+	dialog --title "Jobs/Threads" --radiolist "Choose number of jobs/threads" $NV $NH 10 \
+		1 "1 thread (one job at a time)" off \
+		2 "2 threads" off \
+		4 "4 threads" on \
+		8 "8 threads" off \
+		2>$TMPFILE
+	NJOBS=$(cat $TMPFILE)
+fi
 
 # Confirm build options
-dialog --title "$0" --yesno "You chose to build $TARGET with $NJOBS jobs. Is this okay?\n\nPress No to cancel build." $NV $NH
+dialog --title "Confirm" --yesno "You chose to build $TARGET with $NJOBS jobs. Is this okay?\n\nPress No to cancel build." $NV $NH
 YESNO=$?
 if [ $YESNO -eq 0 ]; then
 	if [ $MAKECMD = "rake" ]; then JOBSOPT=""; NJOBS=""; fi
-	dialog --prgbox "$MAKECMD $TARGET $JOBSOPT $NJOBS" $NV $NH
+	dialog --title "Building target" --prgbox "$MAKECMD $TARGET $JOBSOPT $NJOBS" $NV $NH
 fi
 
 # Remove temporary file
