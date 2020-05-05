4f6230c72288a71941ff1b726a6a4f2ed9bee1da
cr0sd <jumpingkanga@outlook.com>
2020-05-05 11:21:29 -0500

    build.sh: CPU load limit options

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ TMPFILE=._buildconfig
 NJOBS=4
 TARGET="all"
 JOBSOPT="-j"
+CPULIMOPT=""
+CPULIM=""
 
 # Check if dialog is found on system
 if [ $(which dialog > /dev/null) -ne 0 ]; then
@@ -27,23 +29,57 @@ dialog --title "Target" --radiolist "Choose target" $NV $NH 10 \
 	2>$TMPFILE
 TARGET=$(cat $TMPFILE)
 
-# Choose multi-threading/jobs options
+# Make utility specific
 if [ $MAKECMD = "make" ]; then
+
+	# MAKE SPECIFIC -----
+	# Choose multi-threading/jobs options
 	dialog --title "Jobs/Threads" --radiolist "Choose number of jobs/threads" $NV $NH 10 \
 		1 "1 thread (one job at a time)" off \
 		2 "2 threads" off \
 		4 "4 threads" on \
 		8 "8 threads" off \
+		Infinite "No limit" off \
+		CPU "Limit by CPU load..." off \
 		2>$TMPFILE
 	NJOBS=$(cat $TMPFILE)
+
+	# Specify CPU load option if chosen
+	if [ $NJOBS = "CPU" ]; then
+
+		# Don't use '-j' options
+		NJOBS=""
+		JOBSOPT=""
+
+		# Get CPU load limit, validate input
+		CPULIM="invalid"
+		while true; do
+			# Choose CPU load limit
+			dialog --title "CPU Load Limit" --inputbox "Choose CPU load limit for make$WARN" $NV $NH "1.0" 2> $TMPFILE
+			CPULIM=$(cat $TMPFILE)
+			if [[ $CPULIM =~ ^[0-9]*[\.]?[0-9]+$ ]]; then break; fi
+			WARN="\n\nInvalid. Please enter valid CPU load (e.g., 1.0, 2.5, ...)"
+		done
+
+		CPULIMOPT="-l"
+	fi
+
+else
+
+	# RAKE SPECIFIC -----
+	JOBSOPT=""
+	NJOBS=""
+	CPULIM=""
+	CPULIMOPT=""
 fi
 
 # Confirm build options
 dialog --title "Confirm" --yesno "You chose to build $TARGET with $NJOBS jobs. Is this okay?\n\nPress No to cancel build." $NV $NH
 YESNO=$?
 if [ $YESNO -eq 0 ]; then
-	if [ $MAKECMD = "rake" ]; then JOBSOPT=""; NJOBS=""; fi
-	dialog --title "Building target" --prgbox "$MAKECMD $TARGET $JOBSOPT $NJOBS" $NV $NH
+
+	dialog --title "Building target" \
+		--prgbox "$MAKECMD $TARGET $JOBSOPT $NJOBS $CPULIMOPT $CPULIM" $NV $NH
 fi
 
 # Remove temporary file
